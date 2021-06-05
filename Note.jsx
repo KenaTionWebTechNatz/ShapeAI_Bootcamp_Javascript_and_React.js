c4eb497d4733a3486f879fc59c794f14db70709a
KenaTionWebTechNatz <65387819+KenaTionWebTechNatz@users.noreply.github.com>
2021-06-05 14:28:44 +0300

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js Bootcamp</h1>
+      <p>
+        Hi im Keith Onyango This was an amaizing bootcamp by Shaurya Sinha where
+        i was able to cover everything from scratch on Javascript , React.js,
+        HTML. Grateful for the oppprtunity and hope to share out my skills with
+        others.
+      </p>
+    </div>
+  );
+}
+export default Note;
